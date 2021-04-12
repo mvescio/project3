59e2253b7a99693fc67b9da2d56442c1723fced9
JonnyFuego <79720988+JonnyFuego@users.noreply.github.com>
2021-04-12 09:30:52 -0500

    Added files for front end demo

---
+++ FrontEnd/FrontEndDijkstras.java
@@ -0,0 +1,308 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.zip.DataFormatException;
+
+public class FrontEndDijkstras {
+
+	/**
+	   * @param args
+	   * @throws FileNotFoundException throws FileNotFoundException while implementing BackendInterface
+	   * @throws IOException           throws IONotFoundException while implementing BackendInterface
+	   * @throws DataFormatException   throws DataNotFoundException while implementing BackendInterface
+	   */
+	  public static void main(String[] args)
+	      throws FileNotFoundException, IOException, DataFormatException {
+		  
+	    CS400Graph<String> graph = new CS400Graph<String>();
+	    graph.insertVertex("Dejope Hall");
+	    graph.insertVertex("Camp Randall");
+	    graph.insertVertex("Union South");
+	    graph.insertVertex("Memorial Union");
+	    graph.insertVertex("Van Vleck Hall");
+	    graph.insertVertex("Humanities");
+	    graph.insertVertex("Witte Hall");
+	    graph.insertVertex("Waters Hall");
+	    
+	    
+	    
+	    graph.insertEdge("Dejope Hall", "Camp Randall", 8);
+	    graph.insertEdge("Dejope Hall", "Union South", 7);    
+	    graph.insertEdge("Dejope Hall", "Waters Hall", 3);
+	    
+	    graph.insertEdge("Camp Randall", "Union South", 2);
+	    graph.insertEdge("Camp Randall", "Dejope Hall", 8);
+	    graph.insertEdge("Camp Randall", "Witte Hall", 8);
+	    
+	    graph.insertEdge("Union South", "Dejope Hall", 7);
+	    graph.insertEdge("Union South", "Van Vleck Hall", 4);
+	    graph.insertEdge("Union South", "Witte Hall", 7);
+	    graph.insertEdge("Union South", "Camp Randall", 2);
+	    
+	    graph.insertEdge("Waters Hall", "Dejope Hall", 8);
+	    graph.insertEdge("Waters Hall", "Memorial Union", 8);
+	    graph.insertEdge("Waters Hall", "Van Vleck Hall", 2);
+	    
+	    graph.insertEdge("Van Vleck Hall", "Waters Hall", 2);
+	    graph.insertEdge("Van Vleck Hall", "Memorial Union", 7);
+	    graph.insertEdge("Van Vleck Hall", "Humanities", 6);
+	    graph.insertEdge("Van Vleck Hall", "Union South", 4);
+	    
+	    graph.insertEdge("Memorial Union", "Waters Hall", 8);
+	    graph.insertEdge("Memorial Union", "Humanities", 2);
+	    graph.insertEdge("Memorial Union", "Van Vleck Hall", 7);
+	    
+	    graph.insertEdge("Humanities", "Van Vleck Hall", 6);
+	    graph.insertEdge("Humanities", "Witte Hall", 4);
+	    graph.insertEdge("Humanities", "Memorial Union", 2);
+	    
+	    graph.insertEdge("Witte Hall", "Humanities", 4);
+	    graph.insertEdge("Witte Hall", "Union South", 7);
+	    graph.insertEdge("Witte Hall", "Camp Randall", 8);
+	    
+	    String[] options = new String[graph.vertices.size()];
+	    options[0] = "Camp Randall";
+	    options[1] = "Dejope Hall";
+	    options[2] = "Union South";
+	    options[3] = "Waters Hall";
+	    options[4] = "Van Vleck Hall";
+	    options[5] = "Memorial Union";
+	    options[6] = "Humanities";
+	    options[7] = "Witte Hall";
+
+	    // Welcome message to the user
+	    System.out.println("Welcome to the UW Campus Map!");
+	    System.out.println();
+	    
+	    chooseBoth(graph, options);
+
+	    // Sends the user to menuSelection which enables various functions of the Pokedex
+	    //menuSelection(graph);
+
+	  }
+	  
+	  public static void chooseBoth(CS400Graph<String> graph, String[] options)
+		      throws FileNotFoundException, IOException, DataFormatException {
+		  
+		  System.out.println("Please choose one of the following as your starting point: ");
+		    
+		    
+		    for (int i = 0; i < options.length; i++) {
+		    	System.out.println("[" + i + "]: " + options[i]);
+		    }
+		    
+		    Scanner sc = new Scanner(System.in);
+		    int choiceOfStart = sc.nextInt();
+		    String start = "";
+		    
+		    while (choiceOfStart < 0 || choiceOfStart > 7) {
+		    	System.out.println("Please select a valid starting location.");
+		    	choiceOfStart = sc.nextInt();
+		    }
+		    
+		    start = options[choiceOfStart];
+		    
+		    System.out.println();
+		    System.out.println(start + " is your starting location.");
+		    System.out.println();
+		    
+		    System.out.println("Please choose one of the following as your ending point: ");
+		    
+		    for (int i = 0; i < options.length; i++) {
+		    	System.out.println("[" + i + "]: " + options[i]);
+		    }
+		    
+		    int choiceOfEnd = sc.nextInt();
+		    String end = "";
+		    
+		    while (choiceOfEnd < 0 || choiceOfEnd > 7) {
+		    	System.out.println("Please select a valid ending location.");
+		    	choiceOfEnd = sc.nextInt();
+		    }
+		    
+		    end = options[choiceOfEnd];
+		    
+		    System.out.println();
+		    System.out.println(end + " is your ending location.");
+		    System.out.println();
+		    
+		    System.out.println("Shortest path between " + start + " and " + end + ": ");
+		    System.out.println(graph.shortestPath(start, end));
+		    System.out.println("Total distance: " + graph.getPathCost(start, end) + " yards.");
+		    
+		    System.out.println();
+		    System.out.println("Please select an option: ");
+		    System.out.println("[1] Start Over.");
+		    System.out.println("[2] Change starting point.");
+		    System.out.println("[3] Change ending point.");
+		    System.out.println("[4] Close program.");
+		    
+		    int choice = sc.nextInt();
+		    
+		    while (choice < 1 || choice > 4) {
+		    	System.out.println("Please enter a valid option.");
+		    	choice = sc.nextInt();
+		    }
+		    
+		    if (choice == 1) {
+		    	chooseBoth(graph, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 2) {
+		    	changeStart(graph, end, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 3) {
+		    	changeEnd(graph, start, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 4) {
+		    	System.out.println("Goodbye!");
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    sc.close();
+	  }
+	  
+	  public static void changeStart(CS400Graph<String> graph, String end, String[] options) 
+			  throws FileNotFoundException, IOException, DataFormatException {
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Please choose a new starting location.");
+		  for (int i = 0; i < options.length; i++) {
+		    	System.out.println("[" + i + "]: " + options[i]);
+		    }
+		    
+		  int choiceOfStart = sc.nextInt();
+		    String start = "";
+		    
+		    while (choiceOfStart < 0 || choiceOfStart > 7) {
+		    	System.out.println("Please select a valid starting location.");
+		    	choiceOfStart = sc.nextInt();
+		    }
+		    
+		    start = options[choiceOfStart];
+		  
+		  System.out.println(start + " is your new starting location.");
+		    System.out.println();
+		  
+		  System.out.println("Shortest path between " + start + " and " + end + ": ");
+		    System.out.println(graph.shortestPath(start, end));
+		    System.out.println("Total distance: " + graph.getPathCost(start, end) + " yards.");
+		    
+		    System.out.println();
+		    System.out.println("Please select an option: ");
+		    System.out.println("[1] Start Over.");
+		    System.out.println("[2] Change starting point.");
+		    System.out.println("[3] Change ending point.");
+		    System.out.println("[4] Close program.");
+		    
+		    int choice = sc.nextInt();
+		    
+		    while (choice < 1 || choice > 4) {
+		    	System.out.println("Please enter a valid option.");
+		    	choice = sc.nextInt();
+		    }
+		    
+		    if (choice == 1) {
+		    	chooseBoth(graph, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 2) {
+		    	changeStart(graph, end, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 3) {
+		    	changeEnd(graph, start, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 4) {
+		    	System.out.println("Goodbye!");
+		    	sc.close();
+		    	return;
+		    }
+		    
+		  sc.close();
+	  }
+	  
+	  public static void changeEnd(CS400Graph<String> graph, String start, String[] options) 
+			  throws FileNotFoundException, IOException, DataFormatException {
+		  
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Please choose a new ending location.");
+		  for (int i = 0; i < options.length; i++) {
+		    	System.out.println("[" + i + "]: " + options[i]);
+		    }
+		    
+		  int choiceOfEnd = sc.nextInt();
+		    String end = "";
+		    
+		    while (choiceOfEnd < 0 || choiceOfEnd > 7) {
+		    	System.out.println("Please select a valid ending location.");
+		    	choiceOfEnd = sc.nextInt();
+		    }
+		    
+		    end = options[choiceOfEnd];
+		  
+		  System.out.println(end + " is your new ending location.");
+		    System.out.println();
+		  
+		  System.out.println("Shortest path between " + start + " and " + end + ": ");
+		    System.out.println(graph.shortestPath(start, end));
+		    System.out.println("Total distance: " + graph.getPathCost(start, end) + " yards.");
+		    
+		    System.out.println();
+		    System.out.println("Please select an option: ");
+		    System.out.println("[1] Start Over.");
+		    System.out.println("[2] Change starting point.");
+		    System.out.println("[3] Change ending point.");
+		    System.out.println("[4] Close program.");
+		    
+		    int choice = sc.nextInt();
+		    
+		    while (choice < 1 || choice > 4) {
+		    	System.out.println("Please enter a valid option.");
+		    	choice = sc.nextInt();
+		    }
+		    
+		    if (choice == 1) {
+		    	chooseBoth(graph, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 2) {
+		    	changeStart(graph, end, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 3) {
+		    	changeEnd(graph, start, options);
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    if (choice == 4) {
+		    	System.out.println("Goodbye!");
+		    	sc.close();
+		    	return;
+		    }
+		    
+		    sc.close();
+		  
+	  }
+}
